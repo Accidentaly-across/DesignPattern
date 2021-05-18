ac9c0bd36e45cd076fb54c3140f2ed727a0ae423
summer <liyf1@futongcloud.com.cn>
2021-05-18 22:50:30 +0800

    Initialize

---
+++ DesignPattern/src/main/java/openclose/ICourse.java
@@ -0,0 +1,10 @@
+package openclose;
+
+/**
+ * @auther: liyf
+ * @date: 2021/5/18
+ * @description: 开闭原则
+ * @versions:1.0
+ */
+public interface ICourse {
+}
---
+++ DesignPattern/src/main/java/openclose/JavaCourse.java
@@ -0,0 +1,11 @@
+package openclose;
+
+/**
+ * @auther: liyf
+ * @date: 2021/5/18
+ * @description:
+ * @versions:1.0
+ */
+public class JavaCourse  implements ICourse{
+
+}
